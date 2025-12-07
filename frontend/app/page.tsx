614c80bebcb72c7ad338be80276a7ea5bbb9760d
Atul Dhage <atuldhage2003@gmail.com>
2025-12-07 05:48:05 +0530

    Deployment issues

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/blogs");
-}
+// export default function Home() {
+//   redirect("/blogs");
+// }
+
+import React from "react";
+
+const Home = () => {
+  return <div>This is home page</div>;
+};
+
+export default Home;
